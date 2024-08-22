eb593871b6f4ba8adfa021f483ee45a252723993
orun-sunny <sunny.mukut@gmail.com>
2024-08-22 13:43:01 +0600

    left side

--- src/pages/LeftSideNav/LeftSideNav.jsx
+++ src/pages/LeftSideNav/LeftSideNav.jsx
@@ -2,18 +2,18 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-  // const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("categories.json")
-  //     .then((res) => res.json())
-  //     .then((data) => setCategories(data));
-  // }, []);
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
 
   return (
     <div className="space-y-6">
       <h2 className="text-2xl">All Categories</h2>
-      {/* {categories.map((category) => (
+      {categories.map((category) => (
         <Link
           className="block ml-4 text-xl font-semibold"
           key={category.id}
@@ -21,7 +21,7 @@ const LeftSideNav = () => {
         >
           {category.name}
         </Link>
-      ))} */}
+      ))}
     </div>
   );
 };
